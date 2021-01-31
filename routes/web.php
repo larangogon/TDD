b9747c96f01e7c04b621e9daf3e6a62e100f7d87
larangogon <67110144+larangogon@users.noreply.github.com>
2021-01-30 19:48:06 -0500

    test: carga de archivos

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 Route::get('test', function () {
     return 'hola soy un test';
 });
+
+Route::view('profile', 'profile');
+
+Route::post('profile', function (Illuminate\Http\Request $request) {
+    $request->file('photo')->store('profiles');
+    return redirect('profile');
+});
